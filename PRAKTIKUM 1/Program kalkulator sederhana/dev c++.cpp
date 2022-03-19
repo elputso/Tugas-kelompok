8031306eee074134e4fa8a7f8fa97938891d0a4b
elputso <rezasaputra376386@gmail.com>
2022-03-18 23:16:48 -0700

    tugas kelompok

---
+++ PRAKTIKUM 1/Program kalkulator sederhana/dev c++.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int penambahan (int a, int b)
+{
+	int c=a+b;
+	return c;
+}
+int pengurangan (int d, int e)
+{
+	int f=d-e;
+	return f;
+}
+int perkalian (int g, int h)
+{
+	int i=g*h;
+	return i;
+}
+double pembagian (double j, double k)
+{
+	double l=j/k;
+	return l;
+}
+int pangkat(int m, int n)
+{
+	int o=pow(m,n);
+	return o;
+}
+double akarpangkat(double p)
+{
+	double q=sqrt(p);
+	return q;
+}
+int main()
+{
+	int a,b,d,e,g,h,m,n, menu;
+	double j,k,p;
+	string mulai;
+}
